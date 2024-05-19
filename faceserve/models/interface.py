657329334a718f7d70988b609b1bd668f7c0a763
Hung Nguyen <hungnq131002@gmail.com>
2024-05-19 10:54:14 +0700

    chore: add kwargs for more general and easier to extend

--- faceserve/models/interface.py
+++ faceserve/models/interface.py
@@ -1,27 +1,28 @@
 from typing import List, Any
-from abc import ABC, abstractmethod
+from abc import abstractmethod
 from pathlib import Path
+from pydantic import BaseModel
 
 
-class InterfaceModel(ABC):
+class InterfaceModel(BaseModel):
     '''Interface for Model'''
 
     @abstractmethod
-    def load_model(self, path: Path) -> Any:
+    def load_model(self, path: Path, **kwargs) -> Any:
         ...
 
     @abstractmethod 
-    def preprocess(self, image: Any) -> Any:
+    def preprocess(self, image: Any, **kwargs) -> Any:
         ...
 
     @abstractmethod
-    def postprocess(self, image: Any) -> Any:
+    def postprocess(self, image: Any, **kwargs) -> Any:
         ...
 
     @abstractmethod
-    def get_features(self, image: Any) -> Any:
+    def get_features(self, image: Any, **kwargs) -> Any:
         ...
 
     @abstractmethod
-    def forward(self, images: List[Any]) -> List[Any]:
+    def forward(self, images: List[Any], **kwargs) -> List[Any]:
         ...
--- faceserve/services/interface.py
+++ faceserve/services/interface.py
@@ -7,17 +7,17 @@ class InterfaceService(ABC):
     '''Interface for Face Recognition Service'''
 
     @abstractmethod
-    def get_face_emb(self, images: List[Any]) -> List[Any]:
+    def get_face_emb(self, images: List[Any], **kwargs) -> List[Any]:
         ...
 
     @abstractmethod 
-    def check_face(self, id: str, images: List[Any]) -> List[Any]:
+    def check_face(self, id: str, images: List[Any], **kwargs) -> List[Any]:
         ...
 
     @abstractmethod
-    def register_face(self, id: str, images: List[Any], face_folder: Path) -> List[Any]:
+    def register_face(self, id: str, images: List[Any], **kwargs) -> List[Any]:
         ...
 
     @abstractmethod
-    def validate_face(self, id: str, images: List[Any], thresh: float) -> List[Any]:
+    def validate_face(self, id: str, images: List[Any], **kwargs) -> List[Any]:
         ...
