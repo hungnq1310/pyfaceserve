5df4164a6d94860f26ae511a26821f6cac3a78a3
Hung Nguyen <hungnq131002@gmail.com>
2024-05-29 16:46:05 +0700

    feat: add type checking, detech mix-func of get_face_emb()

--- faceserve/services/v1.py
+++ faceserve/services/v1.py
@@ -2,18 +2,20 @@ import hashlib
 import numpy as np
 from fastapi import HTTPException, status
 from pathlib import Path
+from PIL import Image
+from typing import Tuple, Any, List
 
 from faceserve.db.interface import InterfaceDatabase
-from faceserve.models.interface import InterfaceModel 
+from faceserve.models.interface import InterfaceModel
 
 from .interface import InterfaceService
+
 
 def softmax(x):
     return np.exp(x) / np.sum(np.exp(x))
 
 
 class FaceServiceV1(InterfaceService):
-
     def __init__(
         self,
         detection: InterfaceModel,
@@ -32,37 +34,32 @@ class FaceServiceV1(InterfaceService):
         self.recognition = recognition
         self.recognition_thresh = recognition_thresh
 
-    def get_face_emb(self, images):
-        #
-        if isinstance(images, list):
-            res, imgs, temp = list(), list(), [self.get_face_emb(x) for x in images]
-            for i in range(len(temp)):
-                box, emb = temp[i]
-                if emb is not None:
-                    res.append(emb)
-                    imgs.append(images[i])
-            return res, imgs
-        #
-        else:
-            images = np.array(images)
-            boxes, scores, _, kpts = self.detection.detect(
-                images, get_layer="face", conf_det=self.detection_thresh
-            )
-            if len(boxes) == 1:
-                spoof = self.spoofing.get_features(images, boxes, kpts)
-                spoof = softmax(spoof)[:, 0]
-                if spoof[0] > self.spoofing_thresh:
-                    res = self.recognition.get_features(images, boxes, kpts)
-                    return boxes[0], res[0]
-                else:
-                    return boxes[0], None
-            return None, None
+    def get_face_emb(self, image: Image.Image) -> Tuple[Any, Any]:
+        image = np.array(image)  # type: ignore
+        boxes, _, _, _ = self.detection.inference(
+            image, get_layer="face", det_thres=self.detection_thresh
+        )
+        if len(boxes) == 1:
+            spoof = self.spoofing.inference(image)
+            spoof = softmax(spoof)[:, 0]
+            if spoof[0] > self.spoofing_thresh:  # check fake face
+                res = self.recognition.inference(image)
+                return boxes[0], res[0]
+            else:
+                return boxes[0], None
+        return None, None
 
-    def validate_face(self, id: str, images):
-        if id not in self.facedb.list_person():
+    def validate_face(self, id: str, images: List[Image.Image]):
+        if not self.facedb.list_face(id):
             raise HTTPException(status.HTTP_404_NOT_FOUND, "ID not found.")
-        #
-        res, imgs = self.get_face_emb(images)
+        # get temp result
+        res, imgs, temp = [], [], [self.get_face_emb(x) for x in images]
+        for i in range(len(temp)):
+            _, emb = temp[i]
+            if emb is not None:
+                res.append(emb)
+                imgs.append(images[i])
+
         if len(imgs) >= len(images) / 2:
             return res, imgs
         else:
@@ -71,7 +68,7 @@ class FaceServiceV1(InterfaceService):
                 "Your face images is not valid, please try again.",
             )
 
-    def register_face(self, id: str, images, face_folder: Path):
+    def register_face(self, id: str, images: List[Image.Image], face_folder: Path):
         res, imgs = self.validate_face(id, images)
         #
         folder = face_folder.joinpath(f"{id}")
@@ -82,13 +79,13 @@ class FaceServiceV1(InterfaceService):
             emb, img = res[i], imgs[i]
             hash = hashlib.md5(img.tobytes()).hexdigest()
             #
-            self.facedb.insert_face(id, hash, emb)
+            self.facedb.insert_face(emb, hash, id, "default")
             #
             img.save(folder.joinpath(f"{hash}.jpg"))
             response.append(hash)
         return response
 
-    def check_face(self, id: str, images, thresh):
+    def check_face(self, id: str, images: List[Image.Image], thresh: float):
         res, imgs = self.validate_face(id, images)
         #
         checked = [self.facedb.check_face(id, x, thresh) for x in res]
