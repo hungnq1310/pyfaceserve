5b460299ef378d82bc0fbcbc5906340a62c3a78e
Hung Nguyen <hungnq131002@gmail.com>
2024-05-31 07:31:49 +0700

    chore: add missing attribute and initialize method, add document

--- faceserve/models/antispoofing.py
+++ faceserve/models/antispoofing.py
@@ -2,6 +2,7 @@ from typing import Any, List
 import cv2
 import numpy as np
 import onnxruntime as ort
+from pydantic import Field
 
 from faceserve.utils import crop_image, align_5_points
 from .interface import InterfaceModel
@@ -12,12 +13,17 @@ sess_options.graph_optimization_level = ort.GraphOptimizationLevel.ORT_ENABLE_AL
 
 class SpoofingNet(InterfaceModel):
     """SpoofingNet"""
+    input_name: str = Field(default="input")
+    output_name: str = Field(default="output")
+    model_input_size: tuple = Field(default=(256, 256))
+
 
     def __init__(self, model_path) -> None:
-        self.model = self.load_model(model_path)
-        self.input_name = self.model.get_inputs()[0].name
-        self.output_name = self.model.get_outputs()[0].name
-        _, _, w, h = self.model.get_inputs()[0].shape
+        super().__init__()
+        self._model = self.load_model(model_path)
+        self.input_name = self._model.get_inputs()[0].name
+        self.output_name = self._model.get_outputs()[0].name
+        _, _, w, h = self._model.get_inputs()[0].shape
         self.model_input_size = (w, h)
 
     def load_model(self, path: str) -> ort.InferenceSession:
@@ -31,6 +37,16 @@ class SpoofingNet(InterfaceModel):
         )
 
     def preprocess(self, image, xyxys, kpts):
+        """ Preprocessing function with crops and align keypoints
+
+        Args:
+            image (np.array, optional): input image
+            xyxys (tuple, optional): bbox prediction from headface model
+            kpts (tuple, optional): keypoint prediction from headface model
+
+        Returns:
+            crops (np.array): list of corps (num_crops, 3, 256, 256)
+        """
         crops = []        
         # dets are of different sizes so batch preprocessing is not possible
         for box, kpt in zip(xyxys, kpts):
@@ -51,12 +67,22 @@ class SpoofingNet(InterfaceModel):
         return crops
 
     def inference(self, image, xyxys, kpts):
+        """ Predict if face is fake or real
+
+        Args:
+            image (np.array): input image
+            xyxys (tuple, optional): bbox prediction from headface model
+            kpts (tuple, optional): keypoint prediction from headface model
+
+        Returns:
+            result ([batch, logit_scores]): output of model - size: (batch, 2)
+        """
         if isinstance(image, list):
             image = np.array(image)
         # Add preprocessing
         crops = self.preprocess(image, xyxys, kpts)
         # Create output
-        result = self.model.run(
+        result = self._model.run(
             [self.output_name], {self.input_name: crops.astype("float32")}
         )[0]
         
