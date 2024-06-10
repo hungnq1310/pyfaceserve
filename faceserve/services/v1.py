fix: current softmax not true when dim = 2

faceserve/services/v1.py
@@ -11,8 +11,14 @@ from faceserve.models.interface import InterfaceModel
 from .interface import InterfaceService
 
 
-def softmax(x):
-    return np.exp(x) / np.sum(np.exp(x))
+def softmax(z):
+    assert len(z.shape) == 2
+    s = np.max(z, axis=1)
+    s = s[:, np.newaxis] # necessary step to do broadcasting
+    e_x = np.exp(z - s)
+    div = np.sum(e_x, axis=1)
+    div = div[:, np.newaxis] # dito
+    return e_x / div
 
 
 class FaceServiceV1(InterfaceService):