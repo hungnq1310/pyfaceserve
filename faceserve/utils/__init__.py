c1647e972f877a63cf257d6a6027d9a3bb915db0
tienkng <None>
2024-09-26 10:17:27 +0700

    chores: add import module

--- faceserve/utils/__init__.py
+++ faceserve/utils/__init__.py
@@ -1,9 +1,10 @@
 from .face import align_5_points
-from .image import crop_image, to_base64, from_base64
+from .image import crop_image, to_base64, from_base64, preprocess
 
 __all__ = [
     'align_5_points',
     'crop_image',
     'to_base64',
-    'from_base64'
+    'from_base64',
+    'preprocess'
 ]
