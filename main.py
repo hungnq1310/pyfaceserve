553447d5872e68c6e8e7d64389c731749b1522fe
tienkng <None>
2024-09-27 23:33:42 +0700

    chores: update service v2

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
-from api import v1
-from api.v1 import FACES_IMG_DIR
+from faceserve.api import v2
+from faceserve.api.v2 import FACES_IMG_DIR
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 app.mount("/imgs", StaticFiles(directory=FACES_IMG_DIR), name="imgs")
-app.include_router(v1.router)
+app.include_router(v2.router)
 
 @app.get("/")
 async def root():
